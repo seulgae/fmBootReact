1924596912fada40b588c0e4d2e64f8664c0bac0
Seulgae <r1605866@naver.com>
2025-04-21 20:04:37 +0900

    fmBoot React Project Upload

---
+++ src/main/java/com/seulgae/fmbootreact/FmBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.seulgae.fmbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmBootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmBootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seulgae/fmbootreact/FmBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seulgae.fmbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
